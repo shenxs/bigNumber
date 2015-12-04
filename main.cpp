e31c1717aba08920785516be5d55798e1eddbe5a
shenxs <247820400@qq.com>
2015-12-04 15:05:20 +0800

    计时功能实现了,但是linux和windows的计时精度不一样

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<time.h>
 #include"bigNumber.h"
 #include"node.h"
 #include"list.h"
@@ -24,8 +25,12 @@ int main()
     cout<<"c=a*b="<<c<<endl;
     c=a/b;
     cout<<"c=a/b="<<c<<endl;
-    c=(a^b)/c;
-    cout<<"c=(a^b)/c="<<c<<endl;
+    clock_t start,end;
+    start=clock();
+    c=(a^b);
+    end=clock();
+    cout<<"用时"<<end-start<<"×10^(-6)秒"<<endl;
+    cout<<"c=(a^b)="<<c<<endl;
     if(a==c)
     {
         cout<<"a 等于c"<<endl;
